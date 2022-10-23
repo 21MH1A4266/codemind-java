9784702bf9902a1290314f728125bb10be276f20
21MH1A4266 <106150768+21MH1A4266@users.noreply.github.com>
2022-10-23 12:35:51 +0530

    import java.util.Scanner; class Multi { public static void main(String args[])  {
       Scanner s=new Scanner(System.in);
       float a=s.nextFloat();
       float b=s.nextFloat();
       float c=a*b;
    System.out.println(""+ String.format("%.2f",+c));
     
     }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Multi
+{
+public static void main(String args[])
+ {
+   
+   Scanner s=new Scanner(System.in);
+   float a=s.nextFloat();
+   float b=s.nextFloat();
+   float c=a*b;
+System.out.println(""+ String.format("%.2f",+c));
+ 
+ }
+}
