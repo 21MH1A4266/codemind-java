b7b3c3d768c67ef9ecd15f5e3f7a9cd6d5141be2
21MH1A4266 <106150768+21MH1A4266@users.noreply.github.com>
2022-10-24 10:24:20 +0530

    import java.util.Scanner; import java.lang.Math; class Disk {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a,b,c;         int s;
            a=sc.nextInt();
            b= sc.nextInt();
            c= sc.nextInt();
            s = a*b*c*2*512;
            
            System.out.println(s);
            
        }
        
     }

---
+++ Disk_Capacity.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Disk
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        int s;
+        
+        a=sc.nextInt();
+        b= sc.nextInt();
+        c= sc.nextInt();
+        s = a*b*c*2*512;
+        
+        System.out.println(s);
+        
+    }
+    
+ }
